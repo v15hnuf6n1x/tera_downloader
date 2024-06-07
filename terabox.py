eea9515ee30fbcb52716a7e0ba46d22c717e598e
VISHNU <Vishnuvishva2434@gmail.com>
2024-06-07 14:25:32 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -52,8 +52,9 @@ async def start_command(client, message):
     user_mention = message.from_user.mention
     reply_message = f"ᴡᴇʟᴄᴏᴍᴇ, {user_mention}.\n\n🌟 ɪ ᴀᴍ ᴀ ᴛᴇʀᴀʙᴏx ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ʙᴏᴛ. sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴛᴇʀᴀʙᴏx ʟɪɴᴋ ɪ ᴡɪʟʟ ᴅᴏᴡɴʟᴏᴀᴅ ᴡɪᴛʜɪɴ ғᴇᴡ sᴇᴄᴏɴᴅs ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜ ✨."
     join_button = InlineKeyboardButton("ᴊᴏɪɴ ", url="https://t.me/teraboxx_downloader")
-    developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ⚡️", url="https://t.me/naresh3221")
-    reply_markup = InlineKeyboardMarkup([[join_button, developer_button]])
+    developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ⚡️", url="https://t.me/naresh3221")
+    help_button = InlinekeyboardButton("Help",url="https://t.me/teraboxx_downloader")
+    reply_markup = InlineKeyboardMarkup([[join_button, developer_button, help_button]])
     await message.reply_text(reply_message, reply_markup=reply_markup)
 
 async def is_user_member(client, user_id):
