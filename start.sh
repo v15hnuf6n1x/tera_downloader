ecda7e486fc408066b154e60036141ee540b5af9
VISHNU <Vishnuvishva2434@gmail.com>
2024-06-07 23:38:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-aria2c --enable-rpc --rpc-listen-all=true --rpc-listen-port=6800 --rpc-allow-origin-all && python terabox.py
-
+aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon && python terabox.py
